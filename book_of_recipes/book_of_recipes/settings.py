f027f11f56e4dacef445094eee4a143ad7eceece
Natalya <117934761+Adsorbchia@users.noreply.github.com>
2024-04-02 19:12:29 +0400

    added requirements.txt

--- book_of_recipes/book_of_recipes/settings.py
+++ book_of_recipes/book_of_recipes/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from django.conf.global_settings import LOGOUT_REDIRECT_URL
@@ -22,13 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w9u0-1^f4cnnv95&mzl7ru+&g9^k^(z)66kzcs2)*=g6^bl@(t'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',]
-
+ALLOWED_HOSTS = [
+    # '127.0.0.1',
+    'natniks.pythonanywhere.com',
+    ]          
+SESSION_COOKIE_SECURE = True  
+CSRF_COOKIE_SECURE = True
 
 # Application definition
 
@@ -83,13 +87,25 @@ WSGI_APPLICATION = 'book_of_recipes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES = {  
+    "default": {  
+        "ENGINE": "django.db.backends.mysql",  
+        "NAME": "natniks$mydatabase",  
+        "USER": "natniks",  
+        "PASSWORD": os.getenv("MYSQL_PASSWORD"),  
+        "HOST": "natniks.mysql.pythonanywhere-services.com",  
+        "OPTIONS": {  
+            "init_command": "SET NAMES 'utf8mb4';SET sql_mode = 'STRICT_TRANS_TABLES'",  
+            "charset": "utf8mb4",  
+        },  
+    }  
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -126,8 +142,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-
 STATIC_ROOT = BASE_DIR / 'static/' 
 
 MEDIA_URL = 'media/'
